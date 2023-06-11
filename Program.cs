67ecd7f0e9ef35f2345683e967b90b65aa24b3c0
Guadalupe Atim <guadiiatim1309@gmail.com>
2023-06-11 01:03:19 -0300

    archivo csv

--- Program.cs
+++ Program.cs
@@ -6,11 +6,18 @@ string? ruta;
 Console.WriteLine("Ingrese la ruta de una carpeta: ");
 ruta = Console.ReadLine();
 
-List<string> archivos = Directory.GetDirectories(ruta).ToList();
+//listar archivos
+//List<string> archivos = Directory.GetFiles(ruta).ToList();
+List<string> archivos = Directory.EnumerateFiles(ruta).ToList();
+
+string? ruta2 = @"C:\taller1\tl1_tp8_2023-guadaatim\";
+
+StreamWriter index = new StreamWriter(ruta2 + "index.csv", true);
 
 foreach (string archivo in archivos)
 {
     Console.WriteLine(archivo);
+    index.WriteLine(archivo);
 }
 
-
+index.Close();
